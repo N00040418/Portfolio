6a44699721c5f54043f77b52318e216bd5b941d1
Jonathan Schratter <jd7habibi@gmail.com>
2021-10-02 01:19:01 -0400

    First Draft

--- routes/router.js
+++ routes/router.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get("/", (req,res) => {
     res.render('index');
 });
+router.get("/portfolio", (req,res) => {
+    res.render('portfolio');
+});
 
 module.exports = router;
